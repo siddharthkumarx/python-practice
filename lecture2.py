36150f2bd5ecfda8e1883f5771f2f1fa3d2657dd
Siddharth Kumar <273157498+siddharthkumarx@users.noreply.github.com>
2026-06-21 17:00:51 +0530

    Comment out all code in lecture2.py
    Commented out the entire content of the lecture2.py file, preserving the original code structure for future reference.

--- lecture2.py
+++ lecture2.py
@@ -1,1 +1,171 @@
+# # marks = int(input("Enter your marks: "))
+
+# # if(marks >= 90):
+# #     print("A")
+# # elif(marks<90 and marks>=80 ):
+# #     print("B")
+# # elif(marks < 80 and marks >= 70):
+# #     print("C")
+# # elif(marks<70):
+# #     print("D")
+
+# marks = int(input("enter your marks: "))
+
+# if (marks >= 90):
+#     grade = "A"
+# elif (marks >= 80 and marks < 90):
+#     grade = "B"
+# elif (marks >= 70 and marks < 80):
+#     grade = "C"
+# elif(marks <70):
+#     grade ="D"
+
+# print("Student Grade: ", grade)
+
+
+# age = int(input("enter your age: "))
+
+# if(age >= 18):
+#     if(age > 80):
+#         print("Cannot drive")
+#     else:
+#         print("can Drive")
+# else:
+#     print("can drive")
+
+# age = int(input("Enter Your Age: "))
+
+# if(age >= 18):
+#     if(age >80):
+#         print("cannot drive")
+#     else:
+#         print("Dhoom Machaleee")
+# else:
+#     print("nikal tu")
+
+
+# age = int(input("enter your age: "))
+
+# if(age >= 18):
+#     if(age >60):
+#         print("Nikal Budde")
+#     else:
+#         print("ja Drive Kr le")
+# else:
+#     print("Tu nikal")
+
+
+# light = input("Traffic Colour: ")
+
+# if(light =="green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("Slow")
+# elif(light == "red"):
+#     print("Stop ")
+# else:
+#     print("light is BKL")
+
+# marks = int(input("Enter your marks: "))
+
+# if(marks>= 90):
+#     grade ="A"
+# elif(marks >= 80 and marks <90):
+#     grade ="B"
+# elif(marks >=70 and marks < 80):
+#     grade ="C"
+# else:
+#     grade ="D"
+
+# print("Student Grade: ", grade)
+## If one extra space before print have, then all code broke
+
+# ------------------------------------------------------------------------
+# Question 1
+# name =input("Enter your First name: ")
+# print("Length of your First name: ", len(name))
+
+#question 2 
+# str1 = "Kr$na Rap song, $ sign money flow $$$$"
+# money = str1.count("$")
+# print(money)
+
+#question 3
+# n = int(input("Enter your number: "))
+# if(n%2==0):
+#     num = "EVEN"
+# else:
+#     num = "ODD"
+# print("Your Number is: ", num)
+# -----------------------------
+#optimised version by me , first attempt
+
+# n = int(input("Enter your number: "))
+# if(n%2==0 and n!= 0):  
+#     num = "EVEN"
+# elif(n==0):
+#     num = "Neither Even nor odd"   
+# else:
+#     num = "ODD"
+# print("Your Number is: ", num)
+
+#question 5
+# n = int(input("Enter your number: "))
+
+# if(n%7 == 0 and n!=0):
+#     num = "yes"
+# else:
+#     num = "no"
+# print("multiple of 7 or not: ", num)
+
+#question4
+
+# a= int(input("a: "))
+# b =int(input("b: "))
+# c =int(input("c: "))
+
+# if(a>=b and a>=c):
+#     great = (a, "1st No.") 
+# elif(b>=a and b>=c):
+#     great = (b,"2nd No.")
+# elif(c>=a and c>=b):
+#     great = (c,"3rd No.")
+# print("Greatest Number: ", great)
+
+# or
+# a =int(input("a: "))
+# b= int(input("b: "))
+# c= int(input("c: "))
+# d= int(input("d: "))
+
+# if(a>=b and a>=c and a>=d):
+#     greater = (a, "1st No.")
+# elif(b>=c and b>=d):
+#     greater =(b, "2nd no.")
+# elif(c>=d):
+#     greater = (c, "3rd no.")
+# else:
+#     greater = (d, "4th no.")
+# print("Greatest Number: ", greater)
+
+
+#nesting: subset subset
+
+# age = int(input("age: "))
+
+# if(age>=18):
+#     if(age>80):
+#         print("cannot Drive")
+#     else: 
+#         print("Drive")
+# else:
+#     print("cannot Drive")
+
+
+
+
+
+
+
+
 
