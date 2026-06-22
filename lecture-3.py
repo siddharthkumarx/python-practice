9cc55820f1f1183dee7d2a9cec5349ca3fa4a577
Siddharth Kumar <273157498+siddharthkumarx@users.noreply.github.com>
2026-06-22 15:37:33 +0530

    Remove commented-out code in lecture-3.py

--- lecture-3.py
+++ lecture-3.py
@@ -1,1 +1,87 @@
 #python lecture 3 practice
+# #list
+# marks1 = 51.2
+# marks2= (52.3)
+# marks3= [125.2]
+# marks =(51.2, 52.2, 53.6)
+# print(type(marks))
+# ----------------------------------------
+# #try slicing , i dont count or know last value index, so i use len function 
+# for getting last Value
+    
+# marks =[51.2, 216.5, 2112.26, 2124.2]
+
+# ch = len(marks)
+# print(marks[ 2: ch])
+# ------------------------------------
+
+# print(marks)
+# print(type(marks))
+# print(len(marks))
+# print(marks[00])
+# print(marks[1])
+
+# student =["siddharth", 99, 26, "bilaspur"]
+# print("name of student: ",student[2])
+
+
+# list methods: mutable: we also change index value with any values
+# list = [2, 1, 3]
+# list[2] = 8
+# print(list)
+
+# list.append(4)
+# # list.sort(reverse= True)
+# list.reverse()
+# list.insert(2,5)
+# print(list)
+
+# tuple: imutable: not replace values. difference with str  or int is , comma using
+
+# tup= ('a', 'b', 'c')
+# print(tup)
+# print(type(tup))
+
+# movie1 = input("Enter 1st movies Names:")
+# movie2 = input("2nd movie : ")
+# movie3 = input("3rd Movie: ")
+
+# movies =[movie1, movie2, movie3]
+
+# print(movies)
+# print(type(movies))
+
+# or 
+
+# movies=[]
+# movies.append( input("Enter 1st movies Names:"))
+# movies.append( input("Enter 2nd movies Names:"))
+# movies.append( input("Enter 3rd movies Names:"))
+# print(movies)
+
+# list = ['m', 'n', 'a','m'] option 2
+# list = [1,2,3,2,3,1]
+# list2 = list.copy() 
+# list2.reverse() 
+# if(list2 == list):
+#      print("its palindrome")
+# else:
+#      print("not a palindrome")
+
+# question 3
+# grade = ["C","D","A","A","B","B","A"]
+
+# print(grade.count("A")
+
+#question4 
+
+# grade = ["C","D","A","A","B","B","A"]
+# grade.sort()
+# print(grade)
+
+# tuple and lsit same hi h bs ek changable h(list) and dusra nhi hai
+
+
+
+
+
